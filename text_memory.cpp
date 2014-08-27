6ee3c520ef1e81ed7634451d445c0b8e95aeb020
Miles Shung <chao.xiang@yahoo.com>
2014-08-27 14:30:38 +0800

    Create text_memory.cpp

---
+++ text_memory.cpp
@@ -0,0 +1,102 @@
+/*** Function: Create Memory Neuron
+**** Author  : Chao Xiang
+**** Version : 2014
+**** 
+**/
+#include <iostream>
+#include "memory_network.cpp"
+#include "debug/debugger.h"
+#include <string>
+
+class TextMemory
+{
+	private:
+		// N is defined memory usage limit
+		string data[N];
+		MemoryNetwork network;
+	public:
+		TextMemory()
+		{
+			network = MemoryNetwork();
+		}
+		
+		~TextMemory()
+		{
+			// some memory release process goes here
+		}
+	
+		// return processing result status: true->success, false->failure
+		bool train(string trainee)
+		{
+			if(this==0x000000)
+			{
+				cout<<"Error Occurs In Function [train], Object is Null!"<<endl;
+				return false;
+			}
+			// check if memory exceeds limit
+			long len = network.get_length();
+			if(len>=N)
+			{
+				cout<<"Warning: Network Memory Usage is Full, Cannot Train New Element!"<<endl;
+				return false;
+			}
+			// if trainee matched with any data item
+			long id = 0;
+			bool find = false;
+			
+			for(int i=0;i<len;++i)
+			{
+				if(data[i]==trainee)
+				{
+					find = true;
+					id = i;
+					break;
+				}
+			}
+			
+			if(!find)
+			{
+				// this is a new element
+				if(len==N-1)
+				{
+					cout<<"Warning: Network Memory Usage is Full, Cannot Train New Element!"<<endl;
+					return false;
+				}
+				// add new element
+				id = len;
+			}
+			data[id] = trainee;
+			network.train(id);
+			
+			return true;
+		}
+		
+		
+		// a debug tool function
+		bool dump()
+		{
+			if(this==0x000000)
+			{
+				cout<<"Error Occurs In Function [train], Object is Null!"<<endl;
+				return false;
+			}
+			
+			long len = network.get_length();
+			for(int i=0;i<len;++i)
+			{
+				cout<<"neuron [value, conn] at "<<i<<"-"<<data[i]<<"-"<<network.get_conn(i)<<endl;
+			}
+			return true;
+		}
+		
+		// predict the next one
+		string guess()
+		{
+			trace("Entering number_memory.guess");
+			long cid = network.guess()->get_value();
+			return data[cid];
+		}
+		
+};
+
+
