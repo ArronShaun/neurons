63c468e55bff304841bb1ba4e014f035c5024e8a
Miles Shung <chao.xiang@yahoo.com>
2014-08-27 14:34:17 +0800

    Create DEBUG_debugger.h

---
+++ DEBUG_debugger.h
@@ -0,0 +1,11 @@
+#include <string>
+
+#ifndef _DEBUG_
+	//#define _DEBUG_ 0x000001
+#endif
+
+// define debug method
+void trace(const char*);
+void trace(int);
+void trace(long);
+void trace(void);
