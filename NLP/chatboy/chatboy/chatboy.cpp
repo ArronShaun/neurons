9106ae1f29afe8f5ef966c8a8146c1c29fbb46a0
Miles Shung <chao.xiang.cs@gmail.com>
2014-10-02 02:40:05 -0400

    create NPL sub-project,NPL.

---
+++ NLP/chatboy/chatboy/chatboy.cpp
@@ -0,0 +1,18 @@
+// chatboy.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "chaboy is a project that trying to give a feeling that robots can talk just like human. R u ready, let\'s go!"<<std::endl;
+	// install 
+
+
+
+	// wait for user to interupt the chat
+	getchar();
+
+	return 0;
+}
+
