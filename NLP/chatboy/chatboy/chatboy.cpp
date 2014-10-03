117d021dd159d82bdfb1925647ab42152bbf066d
Arron <arronlovescoding@gmail.com>
2014-10-03 10:33:08 -0400

    chatboy

--- NLP/chatboy/chatboy/chatboy.cpp
+++ NLP/chatboy/chatboy/chatboy.cpp
@@ -1,12 +1,14 @@
 // chatboy.cpp : Defines the entry point for the console application.
-//
+// chatboy is an instance of network
 
 #include "stdafx.h"
+
+#include "../network.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::cout << "chatboy is a project that trying to give a feeling that robots can talk just like human. R u ready, let\'s go!"<<std::endl;
-	// install 
+	// get an instance of network
 
 
 
