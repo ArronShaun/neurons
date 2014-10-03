7f1f380045a58c8ca22175a50aa96a432a25bbbf
Arron <arronlovescoding@gmail.com>
2014-10-03 10:34:20 -0400

    indent modification

--- NLP/chatboy/network.h
+++ NLP/chatboy/network.h
@@ -17,21 +17,21 @@ private:
 	network_mode_t mode;
 
 public:
-	void initialize();					// initialize the network with some random state
-	void switchmode();					// switch from chat mode to practice moode or inversly
-	bool practice(std::string str);			// practice the network by original text
+	void initialize();							// initialize the network with some random state
+	void switchmode();							// switch from chat mode to practice moode or inversly
+	bool practice(std::string str);				// practice the network by original text
 	std::string chat(std::string str);			// request with text and response with text
-	std::string compose();					// compose a new line all by herself
+	std::string compose();						// compose a new line all by herself
 
 	std::string rate(std::string str);			// rate the response, and she response back
-	bool checkstate();					// check her state, normally sleep, digest, listen, compose
-	bool sleep();						// make her sleep and do nothing, all arguments are stored in disk
-	bool wakeup();						// make her wake up and start listening to request
-	bool takearest();					// mode switch to rest and begin to digest things learnt in daytime
+	bool checkstate();							// check her state, normally sleep, digest, listen, compose
+	bool sleep();								// make her sleep and do nothing, all arguments are stored in disk
+	bool wakeup();								// make her wake up and start listening to request
+	bool takearest();							// mode switch to rest and begin to digest things learnt in daytime
 
-	bool selfheal();					// fix internal network, check network completeness.
-	bool forget(std::string keywords);		// forget staff concerning keywords
-	bool reset();						// reset memory of network
+	bool selfheal();							// fix internal network, check network completeness.
+	bool forget(std::string keywords);			// forget staff concerning keywords
+	bool reset();								// reset memory of network
 
 };
 
